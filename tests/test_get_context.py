b57bd6e39e814b08a62b3203f772d0f5be0384cc
christaborium <dxdstudio@gmail.com>
2017-01-27 23:07:08 -0800

    Add tests

---
+++ tests/test_get_context.py
@@ -0,0 +1,23 @@
+"""Tests."""
+
+import pytest
+
+import type_mappers
+
+
+def test_get_all_context_fields():
+    """Test using the getter function returns the right fields."""
+    for ctx, mappings in type_mappers.all_maps.items():
+        for fieldname, mapping in mappings.items():
+            if mapping is not None:
+                assert type_mappers.get_context_field(
+                    fieldname, ctx) == mapping
+
+
+def test_get_not_implemented_fields():
+    """Test using the getter function throws errors."""
+    for ctx, mappings in type_mappers.all_maps.items():
+        for fieldname, mapping in mappings.items():
+            if mapping is None:
+                with pytest.raises(NotImplementedError):
+                    type_mappers.get_context_field(fieldname, ctx) == mapping
--- tests/test_mappers.py
+++ tests/test_mappers.py
@@ -6,13 +6,12 @@ import type_mappers
 def test_all_context_fields_match():
     """Ensure all ctxs have the same number of keys for consistency."""
     counts = dict()
-    for ctx, mapping in type_mappers.all_maps.items():
-        mappings = type_mappers.all_maps[ctx]
+    for ctx, mappings in type_mappers.all_maps.items():
         counts[ctx] = len(mappings)
 
     first_ctx_count = list(counts.items())[0][1]
 
-    for ctx, mapping in type_mappers.all_maps.items():
+    for ctx, mappings in type_mappers.all_maps.items():
         err_msg = '"{}" does not match the length of others: {}'.format(
             ctx, counts)
         assert len(type_mappers.all_maps[ctx]) == first_ctx_count, err_msg
@@ -22,8 +21,7 @@ def test_all_context_fields_in_order():
     """Ensure all ctxs have the same fields in the same order."""
     mappings = type_mappers.all_maps.values()
     counts = []
-    for ctx, mapping in type_mappers.all_maps.items():
-        mappings = type_mappers.all_maps[ctx]
+    for ctx, mappings in type_mappers.all_maps.items():
         counts.append([ctx, sorted(mappings)])
 
     for i, item in enumerate(counts):
@@ -34,3 +32,11 @@ def test_all_context_fields_in_order():
                 fst[0], len(fst[1]), snd[0], len(snd[1])
             )
             assert fst[1] == snd[1], err_msg
+
+
+def test_all_mappings_are_callable():
+    """Test functions."""
+    for ctx, mappings in type_mappers.all_maps.items():
+        for name, mapping in mappings.items():
+            if mapping is not None:
+                assert hasattr(mapping, '__call__')
